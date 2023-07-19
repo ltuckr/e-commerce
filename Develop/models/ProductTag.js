3d45595d662d7e2bf1f22926d1311267801bbb56
ltuckr <l.tucker.rdh@gmail.com>
2023-07-18 21:32:22 -0400

    corrected typo in existing tag-route

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -15,14 +15,14 @@ ProductTag.init(
     },
     product_id: {
       type: DataTypes.INTEGER,
-      referneces: {
+      references: {
         model: "product",
         key: "id",
       },
     },
     tag_id: {
       type: DataTypes.INTEGER,
-      referneces: {
+      references: {
         model: "tag",
         key: "id",
       },
